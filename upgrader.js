a3e4b5ccc484009300bdfd4751bcb7d7c13c2a4c
James Bates <locke504@gmail.com>
2016-06-26 12:15:38 -0500

    Increased upgrader efficiency

--- upgrader.js
+++ upgrader.js
@@ -1,15 +1,22 @@
 // Upgrader
 module.exports = function (creep) {
-    if(creep.carry.energy == 0) {
-        var sources = creep.room.find(FIND_SOURCES);
-        if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(sources[0]);
+    if (creep.memory.task === 'harvest') {
+        if (creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
+        }
+        else {
+            creep.task = 'upgrade';
         }
     }
     else {
-        if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
             creep.moveTo(creep.room.controller);
-            console.log(creep.name + ': Upgrading the controller');
+        }
+        if (creep.carry.energy === 0) {
+            creep.task = 'harvest';
         }
     }
 }

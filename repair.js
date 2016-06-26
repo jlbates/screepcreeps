bf7387b4548dc2ae9979d835fbf59c62f69659d0
James Bates <locke504@gmail.com>
2016-06-26 00:49:53 -0500

    Added repair

---
+++ repair.js
@@ -0,0 +1,14 @@
+// Basic Repair bot
+module.exports = function (creep) {
+	var targets = creep.room.find(Game.STRUCTURES);
+	if(targets.length > 0) {
+      if(target.hits < target.hitsMax)
+  		creep.moveTo(targets[0]);
+  		creep.repair(targets[0]);
+  		console.log(creep.name + ': Repairing ' + targets[0]);
+    }
+    else {
+      creep.moveTo(Game.spawns.Spawn1);
+    }
+  }
+}

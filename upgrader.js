fd47bad6b402c1cb0fa1cac3937908cca87c3916
James Bates <locke504@gmail.com>
2016-06-26 13:42:09 -0500

    Increased upgrader efficiency

--- upgrader.js
+++ upgrader.js
@@ -13,3 +13,33 @@ module.exports = function (creep) {
         }
     }
 }
+
+// // Upgrader
+// module.exports = function (creep) {
+//   creep.memory.upgrading = true;
+//     // Returns false if carried energy is zero
+// 	if(creep.memory.upgrading && creep.carry.energy == 0) {
+//       creep.memory.upgrading = false;
+//       console.log(creep.name + ': Gathering materials to upgrade');
+//   }
+//   //Returns true if carrying max energy
+//   if(!creep.memory.upgrading && creep.carry.energy == creep.carryCapacity) {
+//       creep.memory.upgrading = true;
+//   }
+//   //If creep is at carry capacity find the next construction site and build
+//   if(creep.memory.upgrading) {
+//     if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+//         creep.moveTo(creep.room.controller);
+//         console.log(creep.name + ': Upgrading the controller');
+//     }
+//   }
+//   //If creep isn't at max energy, go farm some.
+//   else {
+//       var sources = creep.room.find(FIND_SOURCES);
+//
+//           if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+//               creep.moveTo(sources[0]);
+//           }
+//
+//   }
+// }

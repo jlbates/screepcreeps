40ed437ba0b317b57128733d2a43bc2fdebdf260
James Bates <locke504@gmail.com>
2016-06-25 23:43:07 -0500

    Improved logging for almost all units

--- guard.js
+++ guard.js
@@ -1,7 +1,7 @@
 // Basic Guard
 module.exports = function (creep) {
 	var targets = creep.room.find(Game.HOSTILE_CREEPS);
-	if(target) {
+	if(targets.length > 0) {
 		creep.moveTo(targets[0]);
 		creep.attack(targets[0]);
 		console.log(creep.name + ': Attacking ' + targets[0]);

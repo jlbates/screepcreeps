25ecde9f60358d017cfbf4175db88e8ebf6e3bc7
James Bates <locke504@gmail.com>
2016-06-25 23:34:47 -0500

    Improved unit behavior

--- archer.js
+++ archer.js
@@ -1,7 +1,7 @@
 // Basic Guard
 module.exports = function (creep) {
 	var targets = creep.room.find(Game.HOSTILE_CREEPS);
-	if(target) {
+	if(targets) {
 		creep.moveTo(targets[0]);
 		creep.rangedAttack(targets[0]);
 	}

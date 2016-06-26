424c887983bcf6254f9af3f4f4914bf075063fae
James Bates <locke504@gmail.com>
2016-06-25 23:41:47 -0500

    Improved logging for almost all units

--- archer.js
+++ archer.js
@@ -1,7 +1,7 @@
 // Basic Guard
 module.exports = function (creep) {
 	var targets = creep.room.find(Game.HOSTILE_CREEPS);
-	if(targets) {
+	if(targets.length > 0) {
 		creep.moveTo(targets[0]);
 		creep.rangedAttack(targets[0]);
 		console.log(creep.name + ': Shooting at ' + targets[0]);

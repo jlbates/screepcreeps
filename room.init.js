fc233a139c7cadeccf80eaa0ad03359265a30ba5
James Bates <locke504@gmail.com>
2016-06-27 22:25:29 -0500

    Pauls code

--- room.init.js
+++ room.init.js
@@ -1,24 +1,42 @@
 module.exports = function(room) {
-    var roomMem = Memory.rooms[name];   
+    const
+        util = require('util');
+
+    var roomMem = Memory.rooms[room.name];   
     if(!roomMem) {
-        roomMem = Memory.rooms[name] = {};
+        roomMem = Memory.rooms[room.name] = {};
     }
     
     if(!roomMem.init) {
+        var sources = {};
+        room.find(FIND_SOURCES).forEach(function(source) {
+            sources[creep.id] = {
+                assigned: 0,
+                capacity: getSourceCapacity(source)
+            };
+        });
+
         var creeps = {};
-        
         room.find(FIND_MY_CREEPS).forEach(function(creep) {
             creeps[creep.name] = {};
         });
-        
-        Memory.rooms[name] = {
-            assignments: {
-                harvester: 0,
-                upgrader: 0,
-                builder: 0
-            },
-            creeps: creeps,
-            init: true
-        }
+        roomMem.assignments = {
+            harvester: 0,
+            upgrader: 0,
+            builder: 0
+        };
+        room.creeps = creeps;
+        room.init = true;
+    }
+
+    function getSourceCapacity(source) {
+        var terrain = util.getSurroundingTerrain(source),
+            count = 0;
+        terrain.forEach(function(square) {
+            if(square.terrain === 'swamp' || square.terrain === 'plain') {
+                count++;
+            }
+        });
+        return count;
     }
 }
